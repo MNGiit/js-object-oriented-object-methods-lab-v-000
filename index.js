c08a7cbe3ba35a35472c2dce99fcdc37b05e15d9
Mohammed Noori <kmanoorib@gmail.com>
2019-02-27 03:54:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,12 @@ function BoardMember (name, state, training) {
     return "No, I must disagree";
   };
 
+  this.approve = function(){
+    return "You can do that!";
+  };
   
-  
-  
+  approve()
+returns "You can do that!" ‣
+TypeError: polishedBoardMember.approve is not a function
+    at Context.<anonymous> (test/indexTest.js:35:34)expect(polishedBoardMember.approve()).to.equal("You can do that!")
 }
