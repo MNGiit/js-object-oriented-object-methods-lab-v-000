93723a355677dcd29a15b097fbb96a2346d9f3a3
Mohammed Noori <kmanoorib@gmail.com>
2019-02-27 03:58:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,6 +19,8 @@ function BoardMember (name, state, training) {
     return "You will see great things from Scuber.";
   };
   
+  this.sayHi = function() {
+    return 'Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.';
+  };
+  
 }
-
-
